12b8c61336e13c3ef3059920fac514d30fe183df
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-10-29 17:18:32 -0300

    fix: replace favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,10 @@
 	const { children } = $props();
 </script>
 
+<svelte:head>
+	<link rel="icon" type="image/svg" href="/favicon.svg" />
+</svelte:head>
+
 <div>
 	<Nav />
 	<div class="transition-colors duration-300 max-w-screen-2xl mx-auto">
