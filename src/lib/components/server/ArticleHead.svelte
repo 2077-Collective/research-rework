b1e867ede1372584630169054e359ef6384b8c84
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-10-29 16:41:58 -0300

    chore: update meta tags

--- src/lib/components/server/ArticleHead.svelte
+++ src/lib/components/server/ArticleHead.svelte
@@ -8,36 +8,28 @@
 </script>
 
 <svelte:head>
-	<!-- Essential Meta Tags -->
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="generator" content="2077 Research" />
 
-	<!-- Basic Meta Tags -->
-	<title>{article.title}</title>
 	<meta name="title" content={article.title} />
 	<meta name="description" content={article.summary} />
 
-	<!-- Open Graph Meta Tags -->
-	<meta property="og:type" content="article" />
+	<!-- OG meta tags -->
+	<meta property="og:type" content="website" />
 	<meta property="og:title" content={article.title} />
 	<meta property="og:description" content={article.summary} />
 	<meta property="og:image" content={article.thumb} />
-	<meta property="og:url" content={pageUrl} />
-	{#if article.scheduledPublishTime}
-		<meta property="article:published_time" content={article.scheduledPublishTime} />
-	{/if}
-	{#if article.authors}
-		<meta
-			property="article:author"
-			content={article.authors.map((author) => author.fullName).join(', ')}
-		/>
-	{/if}
 
-	<!-- X Card Meta Tags -->
+	<!-- Twitter meta tags -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={article.title} />
 	<meta name="twitter:description" content={article.summary} />
 	<meta name="twitter:image" content={article.thumb} />
-	<meta name="twitter:url" content={pageUrl} />
-	<meta name="twitter:site" content="2077Research" />
+	<meta name="twitter:url" content={`${pageUrl}/${article.slug}`} />
+	<meta name="twitter:site" content="@2077Research" />
+
+	<!-- General meta tags -->
+	<meta name="title" content={article.title} />
+	<meta name="description" content={article.summary} />
 </svelte:head>
