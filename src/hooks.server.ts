f78de078abb238dec4e95a09077c724b7810e8c9
ndu <chijindujulian@gmail.com>
2024-11-18 09:45:53 +0100

    chore(fix): add fix

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,40 +1,40 @@
 import type { Handle } from '@sveltejs/kit';
 
 function generateNonce() {
-    return crypto.randomUUID();
+  return crypto.randomUUID();
 }
 
 export const handle: Handle = async ({ event, resolve }) => {
+  try {
     const nonce = generateNonce();
     event.locals.nonce = nonce;
 
     const cspDirectives = {
-        'default-src': ["'self'"],
-        'script-src': [
-            "'self'",
-            `'nonce-${nonce}'`,
-            "https://www.googletagmanager.com",
-            "https://www.google-analytics.com"
-        ],
-        'img-src': [
-            "'self'",
-            "https://www.google-analytics.com"
-        ],
-        'connect-src': [
-            "'self'",
-            "https://www.google-analytics.com"
-        ],
+      'default-src': ["'self'"],
+      'script-src': [
+        "'self'",
+        `'nonce-${nonce}'`,
+        "https://www.googletagmanager.com",
+        "https://www.google-analytics.com"
+      ],
+      'img-src': ["'self'", "https://www.google-analytics.com"],
+      'connect-src': ["'self'", "https://www.google-analytics.com"],
     };
 
     const cspString = Object.entries(cspDirectives)
-        .map(([key, values]) => `${key} ${values.join(' ')}`)
-        .join('; ');
+      .map(([key, values]) => `${key} ${values.join(' ')}`)
+      .join('; ');
 
     const response = await resolve(event);
+
     response.headers.set('Content-Security-Policy', cspString);
     response.headers.set('X-Frame-Options', 'SAMEORIGIN');
     response.headers.set('X-Content-Type-Options', 'nosniff');
     response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
 
     return response;
+  } catch (error) {
+    console.error('Error in handle function:', error);
+    throw error;
+  }
 };
--- src/lib/components/server/BaseHead.svelte
+++ src/lib/components/server/BaseHead.svelte
@@ -4,14 +4,13 @@
 	import { onMount } from 'svelte';
 
 	let hasConsent = false;
-
 	const pageUrl = $page.url.origin;
 	const image = `${pageUrl}/preview-image.jpg`;
 	const title = '2077 Research';
 	const description = 'State of the art research on Ethereum and the broader crypto ecosystem';
 
 	let analyticsScript = '';
-	let nonce = $page.locals.nonce;
+	export let nonce: string;
 
 	onMount(() => {
 		const dnt = browser && navigator.doNotTrack === '1';
@@ -67,6 +66,7 @@
 	<meta name="title" content={title} />
 	<meta name="description" content={description} />
 
+	<!--Consent check for Google analytics-->
 	{#if hasConsent}
 		<link rel="preconnect" href="https://www.googletagmanager.com" />
 		<link rel="preconnect" href="https://www.google-analytics.com" />
