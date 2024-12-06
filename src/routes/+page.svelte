6cc0163b0ea456fc65da375a481d5120d8f5a830
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-12-06 16:21:58 -0300

    feat: add hero image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,6 +29,12 @@
 		>
 			State of the art research on Ethereum and the broader crypto ecosystem
 		</h1>
+
+		<img
+			src="/hero.png"
+			class="absolute top-0 right-0 w-1/2 animate-float"
+			alt="Hero illustration"
+		/>
 	</div>
 
 	<ArticleSpotlight article={articles[0]} />
