4504d9052e757c2106b0cd1b556a984bf12d1936
ndu <chijindujulian@gmail.com>
2024-12-16 09:12:56 +0100

    chore(fix): use descriptive names for schemas

--- src/lib/services/article.service.ts
+++ src/lib/services/article.service.ts
@@ -1,18 +1,18 @@
 import {
-	ArticleMetadataArraySchema,
-	ArticleSchema,
-	type Article,
-	type ArticleMetadata
+    ArticleMetadataArraySchema,
+    FullArticleSchema,
+    type Article,
+    type ArticleMetadata
 } from '$lib/types/article';
 
 export const fetchArticles = async (): Promise<ArticleMetadata[]> => {
-	const res = await fetch('https://cms.2077.xyz/api/articles');
-	const body = await res.json();
-	return ArticleMetadataArraySchema.parse(body);
+    const res = await fetch('https://cms.2077.xyz/api/articles');
+    const body = await res.json();
+    return ArticleMetadataArraySchema.parse(body);
 };
 
 export const getArticleBySlug = async (slug: string): Promise<Article> => {
-	const res = await fetch(`https://cms.2077.xyz/api/articles/${slug}`);
-	const body = await res.json();
-	return ArticleSchema.parse(body.data);
+    const res = await fetch(`https://cms.2077.xyz/api/articles/${slug}`);
+    const body = await res.json();
+    return FullArticleSchema.parse(body.data);
 };
