0519c6b871874a0a05874e3312b219fc783af48a
iankressin <ianguimaraes31@gmail.com>
2024-12-17 13:51:33 -0300

    feat: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+    
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-[70vh] text-center px-4">
+	<h1 class="text-9xl font-bold mb-4 font-soehne">{$page.status}</h1>
+	<p class="text-xl mb-8">
+        {#if $page.status === 404}
+            Oops! Looks like this page is on a different chain.
+        {:else if $page.status === 403}
+            Oops! Looks like you don't have access to this page.
+        {:else}
+            Oops! Looks like something went wrong.
+        {/if}
+	</p>
+	<a
+		href="/"
+		class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
+	>
+		Back to mainnet
+	</a>
+</div> 
--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -2,13 +2,21 @@ import { getArticleBySlug } from '$lib/services/article.service';
 import type { PageServerLoad } from './$types';
 import { render } from 'svelte/server';
 import NewsletterBanner from '$lib/components/ui/NewsletterBanner.svelte';
+import { error, redirect } from '@sveltejs/kit';
 
 const OPENING_H2_TAG = '<h1';
 const NEWSLETTER_ID = 'newsletter-banner-container';
 
 /// Includes the newsletter banner in the article content after the second section
 export const load: PageServerLoad = async ({ params }) => {
+	if (params.slug === '404') return
+
 	const article = await getArticleBySlug(params.slug);
+
+	if (!article) {
+		throw redirect(307, '/404');
+	}
+
 	const positionOfFirstSection = article.content.indexOf(OPENING_H2_TAG);
 	const positionOfSecondSection = article.content.indexOf(
 		OPENING_H2_TAG,
